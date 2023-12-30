13334ee6afce4fe8d6f56d02be8a0004dfaa4adf
H4R5H <151930114+g0j0-H4R5H@users.noreply.github.com>
2023-12-30 18:21:02 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,23 @@
 // background.js
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action === "makeRequest") {
-            // Your existing fetch code here, without including the CSRF token
             fetch('https://kawaiipaxx.pythonanywhere.com/api/endpoint/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    // You may include other headers if needed
                 },
                 body: JSON.stringify({
-                    // Your request payload
+               
                 }),
             })
             .then(response => response.json())
             .then(data => {
-                // Handle the response
+               
                 console.log(data);
             })
             .catch(error => {
-                // Handle errors
+          
                 console.error(error);
             });
         }
@@ -28,7 +25,7 @@ chrome.runtime.onMessage.addListener(
 );
 // function getCSRFToken() {
 //     return new Promise((resolve, reject) => {
-//         // Fetch cookies for the target domain
+//         
 //         chrome.cookies.getAll({ domain: 'kawaiipaxx.pythonanywhere.com' }, cookies => {
 //             const csrfCookie = cookies.find(cookie => cookie.name === 'csrftoken');
 
@@ -41,7 +38,7 @@ chrome.runtime.onMessage.addListener(
 //     })
 //     .catch(error => {
 //         console.error('Error fetching CSRF token:', error);
-//         throw error;  // Throw the error to propagate it further
+//         throw error; 
 //     });
 // }
 
@@ -55,89 +52,29 @@ chrome.runtime.onMessage.addListener(
 //                         return;
 //                     }
 
-//                     // Your existing fetch code here, using the retrieved CSRF token
+//                  
 //                     fetch('https://kawaiipaxx.pythonanywhere.com/api/endpoint/', {
 //                         method: 'POST',
 //                         headers: {
 //                             'Content-Type': 'application/json',
-//                             'X-CSRFToken': csrfToken, // Include the CSRF token in the headers
+//                             'X-CSRFToken': csrfToken, 
 //                         },
 //                         body: JSON.stringify({
-//                             // Your request payload
+//                            
 //                         }),
 //                     })
 //                     .then(response => response.json())
 //                     .then(data => {
-//                         // Handle the response
+//                        
 //                         console.log(data);
 //                     })
 //                     .catch(error => {
-//                         // Handle errors
+//                  
 //                         console.error(error);
 //                     });
 //                 })
 //                 .catch(error => {
-//                     // Handle errors from getCSRFToken
-//                     console.error(error);
-//                 });
-//         }
-//     }
-// );
-// function getCSRFToken() {
-//     return fetch('https://kawaiipaxx.pythonanywhere.com/get-csrf-token/', {
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json',
-//         },
-//         credentials: 'include', // Include credentials to allow cookies to be sent
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error(`Failed to fetch CSRF token. Status: ${response.status}`);
-//         }
-//         return response.text();
-//     })
-//     .then(data => data.csrf_token)
-//     .catch(error => {
-//         console.error('Error fetching CSRF token:', error);
-//         throw error;  // Throw the error to propagate it further
-//     });
-// }
-
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         if (request.action === "makeRequest") {
-//             getCSRFToken()
-//                 .then(csrfToken => {
-//                     if (!csrfToken) {
-//                         console.error("CSRF token not found");
-//                         return;
-//                     }
-
-//                     // Your existing fetch code here, using the retrieved CSRF token
-//                     fetch('https://kawaiipaxx.pythonanywhere.com/api/endpoint/', {
-//                         method: 'POST',
-//                         headers: {
-//                             'Content-Type': 'application/json',
-//                             'X-CSRFToken': csrfToken, // Include the CSRF token in the headers
-//                         },
-//                         body: JSON.stringify({
-//                             // Your request payload
-//                         }),
-//                     })
-//                     .then(response => response.json())
-//                     .then(data => {
-//                         // Handle the response
-//                         console.log(data);
-//                     })
-//                     .catch(error => {
-//                         // Handle errors
-//                         console.error(error);
-//                     });
-//                 })
-//                 .catch(error => {
-//                     // Handle errors from getCSRFToken
+//                  
 //                     console.error(error);
 //                 });
 //         }
